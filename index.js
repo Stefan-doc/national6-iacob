e267a78c713775aa98e2388d1a9582660b01deee
siacob <iacob_stefan78@yahoo.com>
2021-04-11 21:41:55 +0300

    pause

--- index.js
+++ index.js
@@ -9,3 +9,30 @@ document.getElementById("logout").addEventListener("click", () => {
   localStorage.removeItem("password");
   window.location = "/";
 });
+
+const breedsListHtml = document.querySelector(".container");
+
+fetch(`https://dog.ceo/api/breeds/list/all`)
+  .then(response => {
+    console.log(response);
+    return response.json();
+  })
+  .then((data) => {
+    
+    console.log(data.message);
+    // const breed = document.createElement("breeds");
+    // const breedMessage = document.createElement("h3");
+    // const breedStatus = document.createElement("p");
+    const breed = data.message;
+    // breed.appendChild(breedMessage);
+    // breed.appendChild(breedStatus);
+
+    breedsListHtml.append(breed);
+
+    // breedMessage.innerText = data.message;
+    // breedStatus.innerText = data.status;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+  
